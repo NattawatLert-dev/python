f96c98a89a0ddf168f5e9026a2899df7759ef65a
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-08-21 20:55:34 +0700

    feat: variables

---
+++ 01-Basics/02-variables.py
@@ -0,0 +1,31 @@
+# variable = ตัวเก็บค่าข้อมูล (string, integer, float, boolean)
+#            ตัวแปรจะทำงานเสมือนเป็นค่าที่มันเก็บอยู่
+
+# String
+first_name = "tle"
+food = "pizza"
+email = "Tle012@fake.com"
+
+print(f"hello {first_name}")
+
+# Integer
+age = 25
+quantity = 3
+num_of_student = 30
+
+print(f"You are {age} years old")
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is {price}")
+
+# Boolean
+is_student = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
