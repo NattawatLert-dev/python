72127f9b6a4a39d1840423d8a1f7a2c6b2dcc562
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-08-22 09:41:15 +0700

    feat: arithmetic & math

---
+++ 01-Basics/05-arithmetic & math.py
@@ -0,0 +1,36 @@
+import math
+
+friend = 5
+
+# friend = friend + 1
+# friend += 1
+# friend = friend - 2
+# friend -= 2
+# friend = friend * 3
+# friend *= 3
+# friend = friend / 2
+# friend /= 2
+# friend = friend ** 2
+# friend **=2
+remainder = friend % 3
+
+print(remainder)
+
+x = 3.14
+y = 9
+z = 5
+
+# result = round(x)     # 3
+# result = abs(y)       # 9
+# result = pow(4, 3)    # 64
+# result = max(x, y, z) # 5
+# result = min(x, y, z) # 3.14
+
+print(math.pi)
+print(math.e)
+# result = math.sqrt(y)  # 3.0
+# result = math.ceil(x)  # 4
+result = math.floor(x) # 3
+
+
+print(result)
