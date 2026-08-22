bc40cfc999b404b10b27fcc0fad1c8e7c8303fb9
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-08-22 09:18:42 +0700

    feat: type casting

---
+++ 01-Basics/03-type casting.py
@@ -0,0 +1,12 @@
+# typecasting = กระบวนการแปลงตัวแปรจากชนิดข้อมูลหนึ่งไปเป็นอีกชนิดข้อมูลหนึ่ง
+#               str(), int(), float(), bool()
+
+name = "Nattawat"
+age = 25
+gpa = 3.2
+is_student = True
+
+print(type(name)) # str
+
+gpa = int(gpa)
+print(gpa) # 3
